8c2c9289f8af466b7f5e6df16735432fa518b817
KaiFan Liu <aizhini53@vip.qq.com>
2019-09-04 19:15:12 +0800

    Crdeit query

--- src/gdut_gpa_query.py
+++ src/gdut_gpa_query.py
@@ -65,6 +65,9 @@ class GDUT_Class(object):
         self._term_list = None
         self._session = requests.session()
 
+        self._credit_requirement = {"公共基础课 ": 62, "专业课 ": 13, "专业基础课 ": 40.5, "实验实习实训 ": 19.5, "设计(论文) ": 23,
+                                    "公共选修课 自然科学与工程技术类": 3, "公共选修课 人文与社会科学类": 9}
+
     def login(self):
         self._account = input("学号: ")
         self._password = input("密码: ")
@@ -100,11 +103,17 @@ class GDUT_Class(object):
         self._v_code = input("输入验证码: ")
 
     def _request_gpa_data(self):
-        cj_ret = self._session.post("http://222.200.98.147/xskccjxx!getDataList.action?xnxqdm=&jhlxdm=&page=1&rows=100&sort=xnxqdm&order=asc")
+        cj_ret = self._session.post(
+            "http://222.200.98.147/xskccjxx!getDataList.action?xnxqdm=&jhlxdm=&page=1&rows=100&sort=xnxqdm&order=asc")
         self._gpa_data = json.loads(cj_ret.content, encoding="utf-8")
         self._term_list = set()
+        self._class_type_list = {}
         for r in self._gpa_data["rows"]:
             self._term_list.add(r["xnxqmc"])
+            key = "{} {}".format(r["kcdlmc"], r["kcflmc"])
+            if key not in self._class_type_list:
+                self._class_type_list[key] = []
+            self._class_type_list[key].append(r)
         self._term_list = list(self._term_list)
         self._term_list.sort()
 
@@ -113,7 +122,7 @@ class GDUT_Class(object):
 
     def show_terms(self):
         print("以下为可以查询的学期: ")
-        for i in range(1, len(self._term_list)+1):
+        for i in range(1, len(self._term_list) + 1):
             print("{} : {}".format(i, self._term_list[i - 1]))
         term = input("输入需要计算的学期(如:1 2,查询第一第二学期): ").split(" ")
         ret = []
@@ -121,10 +130,7 @@ class GDUT_Class(object):
             ret.append(self._term_list[int(r) - 1])
         return ret
 
-    def cal_gpa(self):
-        if self._gpa_data is None:
-            self._request_gpa_data()
-        request_term = self.show_terms()
+    def cal_gpa(self, request_term):
         ret = 0
         xf_total = 0
         for r in self._gpa_data["rows"]:
@@ -136,10 +142,46 @@ class GDUT_Class(object):
 
         print("======== 您在选择的学期的绩点为: 【 {} 】 ========".format(round(ret / xf_total, 2)))
 
+    def cal_credit_requirement(self, request_term):
+        ret = {}
+        cnt = 0
+        req_cnt = 0
+        for key in self._class_type_list.keys():
+            for r in self._class_type_list[key]:
+                if r["xnxqmc"] in request_term:
+                    key = "{} {}".format(r["kcdlmc"], r["kcflmc"])
+                    if key not in ret:
+                        ret[key] = 0
+                    ret[key] += round(float(r["xf"]), 2)
+        for key in ret.keys():
+            cnt += ret[key]
+            if key in self._credit_requirement:
+                req_cnt += self._credit_requirement[key]
+                if self._credit_requirement[key] - ret[key]<=0:
+                    state = "完成"
+                else:
+                    state = "未完成"
+                print("======== 您在选择的学期 {}课程已修为: 【 {} 】 毕业要求为: 【 {} 】 差值为: 【 {} 】 结论为: 【{}】  ========".
+                      format(key, ret[key], self._credit_requirement[key], self._credit_requirement[key] - ret[key], state))
+            else:
+                print("======== 您在选择的学期 {}课程已修为: 【 {} 】 毕业要求为: 【 0 】 差值为: 【 {} 】 结论为: 【{}】 ========".
+                      format(key, ret[key], ret[key], "完成"))
+        print("【======== 您在选择的学期的总学分为: 【 {} 】 需求总学分为: 【 {} 】 ========】".format(cnt, -1 * req_cnt))
+
+    def func_select(self):
+        if self._gpa_data is None:
+            self._request_gpa_data()
+        request_term = self.show_terms()
+        typ = input("计算绩点输入0 计算学分毕业要求输入1: ")
+        if typ == 0:
+            self.cal_gpa(request_term)
+        else:
+            self.cal_credit_requirement(request_term)
+
 
 gdut = GDUT_Class()
 gdut.login()
 while True:
     if gdut.is_init() and "q" == input("回车继续查询,输入q退出"):
         break
-    gdut.cal_gpa()
+    gdut.func_select()
